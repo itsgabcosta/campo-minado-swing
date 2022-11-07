db70bc9122d1a5671258a3e625e85db3b0d5c033
itsgabcosta <gabrielcosta1302@hotmail.com>
2022-11-07 15:51:52 -0300

    meu primeiro projeto

---
+++ src/br/com/gabs/cm/modelo/CampoObservador.java
@@ -0,0 +1,5 @@
+package br.com.gabs.cm.modelo;
+
+public interface CampoObservador {
+	public void eventoOcorreu(Campo campo, CampoEvento evento);
+}
---
+++ src/br/com/gabs/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,33 @@
+package br.com.gabs.cm.visao;
+
+import java.awt.GridLayout;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import br.com.gabs.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+		setLayout(new GridLayout(
+				tabuleiro.getLinhas(), tabuleiro.getColunas()));
+		
+		tabuleiro.paraCadaCampo(cDeCampo -> add(new BotaoCampo(cDeCampo)));
+		tabuleiro.registrarObservador(rDeRegistro -> {
+			
+			SwingUtilities.invokeLater(() -> {
+				if(rDeRegistro.isGanhou()) {
+					JOptionPane.showMessageDialog(this, "Ganhou :)");
+				} else {
+					JOptionPane.showMessageDialog(this, "Perdeu :(");
+				}
+				
+				tabuleiro.reiniciar();
+			});
+		});
+	}
+}
